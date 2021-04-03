3e324e2feaf18b2a81237c8b8c49ff320b889347
a.ugryumov <ugryumov_alex@mail.ru>
2021-04-04 00:12:12 +0300

    Всё готово, кроме процедуры отключения клиента

--- client/src/Client.java
+++ client/src/Client.java
@@ -1,42 +1,61 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Client {
 
-    private static Socket clientSocket;
-    //private static BufferedReader reader;
-    private static BufferedReader in;
-    private static BufferedWriter out;
-
     public static void main(String[] args) {
         try {
+            Socket clientSocket = new Socket("localhost", 8189);
+
+            BufferedReader in  = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
             try {
-                clientSocket = new Socket("localhost", 8189);
+                //Переводим ввод данных в консоль в отдельный поток
+                new Thread(()->{
+                    try {
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                        System.out.println("Введите сообщение : ");
 
-                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                        while( true ) {
+                            String clientMessage = reader.readLine();
+                            if (!clientSocket.isClosed()) {
+                                out.write(clientMessage + "\n");
+                                out.flush();
 
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                                if (clientMessage.equalsIgnoreCase("exit")) {
+                                    in.close();
+                                    out.close();
+                                    clientSocket.close();
 
-                //Пробуем перенести ввод данных в консоль в отдельный поток
+                                    break;
+                                }
+                            } else {
+                                break;
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
 
+                //В основном потоке слушаем сервер
+                while ( !clientSocket.isClosed() ) {
 
-                while (true) {
-                    System.out.println("Сообщение на сервер:");
+                    String serverMessage = in.readLine();
+                    if (serverMessage != null) {
+                        System.out.println("Сервер : " + serverMessage);
 
-                    String clientMessage = reader.readLine(); // ждём пока соощение пишем в консоль
-                    out.write(clientMessage + "\n"); // отправляем сообщение на сервер
-                    out.flush();
-
-                    String serverMessage = in.readLine(); // ждём, что скажет сервер
-                    System.out.println(serverMessage); // получив - выводим на экран
-
-                    if (clientMessage.equalsIgnoreCase("exit")) {
+                        if (serverMessage.equalsIgnoreCase("exit")) {
+                            break;
+                        }
+                    } else {
                         break;
                     }
                 }
 
-            } finally { // в любом случае необходимо закрыть сокет и потоки
+            } finally {
                 System.out.println("Клиент был закрыт...");
                 clientSocket.close();
                 in.close();
